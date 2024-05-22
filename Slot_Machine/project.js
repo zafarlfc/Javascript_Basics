8f238ca8dddc08f17a3d111e40b59c0fdfc1dc37
Zafar Iqbal <zafarlfc>
2024-05-22 23:23:49 +0500

    Added mechanism for user to deposit money and select no of lines to bet on

---
+++ Slot_Machine/project.js
@@ -0,0 +1,38 @@
+// 1. Deposit some money
+// 2. Determine number of lines to bet on
+// 3. Collect a bet amount
+// 4. Spin the slot machine
+// 5. Check if the user won
+// 6. Give the user their winnings
+// 7. play again
+
+const prompt = require("prompt-sync")();
+
+const deposit = () => {
+    while (true) {
+        const deposit_amount = prompt("Enter deposit amount: ");
+        const amount_number = parseFloat(deposit_amount)
+
+        if (isNaN(amount_number) || amount_number <= 0) {
+            console.log("Invalid deposit amount, try again.");
+        } else {
+            return amount_number;
+        }
+    }
+};
+
+const get_no_of_lines_to_bet = () => {
+    while (true) {
+        const lines = prompt("Enter the number of lines to bet on (1-3): ");
+        const number_of_lines = parseFloat(lines)
+
+        if (isNaN(number_of_lines) || number_of_lines <= 0 || number_of_lines > 3) {
+            console.log("Invalid input, try again.");
+        } else {
+            return number_of_lines;
+        }
+    }
+};
+
+const deposit_amount = deposit();
+const number_of_lines = get_no_of_lines_to_bet();

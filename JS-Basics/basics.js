b7c0e6c85d75463755b88b807f54cc76b3120f81
zafarlfc <zafarlfc@outlook.com>
2024-06-30 22:39:21 +0500

    primitive types in javascript

---
+++ JS-Basics/basics.js
@@ -0,0 +1,20 @@
+// Variables
+let name = "Someone";
+console.log(name);
+
+// Constants
+const value = 10;
+console.log(value);
+
+// Primitive Types
+let username = "newuser";
+let number_ = 20;
+let is_eligible = true;
+let not_defined = undefined;
+let selection = null;
+
+console.log(typeof username);
+console.log(typeof number_);
+console.log(typeof is_eligible);
+console.log(typeof not_defined);
+console.log(typeof selection);
